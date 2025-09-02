11ebbe51a15a208ff6ebedaf68be77cdb2947658
mertalev <101130780+mertalev@users.noreply.github.com>
2025-09-02 13:17:40 -0400

    don't close client

--- mobile/lib/infrastructure/repositories/sync_api.repository.dart
+++ mobile/lib/infrastructure/repositories/sync_api.repository.dart
@@ -112,8 +112,6 @@ class SyncApiRepository {
     } catch (error, stack) {
       _logger.severe("Error processing stream", error, stack);
       return Future.error(error, stack);
-    } finally {
-      _client.close();
     }
     stopwatch.stop();
     _logger.info("Remote Sync completed in ${stopwatch.elapsed.inMilliseconds}ms");
